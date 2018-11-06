9aa6eb43e2d63cf233ebe2d298224368eb55a89d
PabloCalvillo <38098277+PabloCalvillo@users.noreply.github.com>
2018-11-06 19:34:26 +0100

    Creada página para añadir jugadores

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AddPlayerPage } from '../add-player/add-player';
 
 @Component({
   selector: 'page-home',
@@ -9,11 +10,14 @@ import { AngularFireAuth } from 'angularfire2/auth';
 export class HomePage {
 
   constructor(public navCtrl: NavController, public afAuth: AngularFireAuth) {
-
   }
   logOut(){
     this.afAuth.auth.signOut().then(()=>{
       console.log("Sesión cerrada con éxito");
     });
+  }
+
+  addPlayerPage() {
+    this.navCtrl.push(AddPlayerPage);
   }
 }
